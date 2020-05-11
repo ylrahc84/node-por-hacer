701fdf6767458814f673f768195817c0cf371402
charly <coleccionista84@gmail.com>
2020-05-10 21:54:54 -0600

    Primer commit

---
+++ README.md
@@ -0,0 +1,9 @@
+##Alicacion de comandos
+
+Este es el ejercicio del curso
+
+Recuerden instalar los paquetes del node
+
+```
+npm install
+```
---
+++ config/yargs.js
@@ -0,0 +1,31 @@
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'Descripcion de la tarea por hacer'
+}
+
+const completado = {
+    default: true,
+    alias: 'c',
+    desc: 'Marca como completado o pendiente la tarea'
+}
+
+
+const argv = require('yargs')
+    .command('crear', 'Crear un elemento por hacer', {
+        descripcion
+    })
+    .command('actualizar', 'Actualizar el estado completado de una tarea', {
+        descripcion,
+        completado
+    })
+    .command('borrar', 'Borrar un registro', {
+        descripcion
+    })
+    .help()
+    .argv;
+
+
+module.exports = {
+    argv
+}
